f29059dec5f70e4649ee80a770dea4a12481b404
Milo <milo.dev.web@gmail.com>
2021-05-05 15:00:44 +0200

    mise à jour

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 
 // Import Capacitor dependencies and get references to the Camera, Filesystem, and Storage plugins
-import { Plugins, CameraResultType, Capacitor, FilesystemDirectory,
-         CameraPhoto, CameraSource } from '@capacitor/core';
+import {
+  Plugins, CameraResultType, Capacitor, FilesystemDirectory,
+  CameraPhoto, CameraSource
+} from '@capacitor/core';
 const { Camera, Filesystem, Storage } = Plugins;
 
+import { Platform } from '@ionic/angular';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +15,12 @@ const { Camera, Filesystem, Storage } = Plugins;
 export class PhotoService {
   public photos: Photo[] = [];
   private PHOTO_STORAGE: string = "photos";
+  private platform: Platform;
 
-  constructor() { }
+
+  constructor(platform: Platform) {
+    this.platform = platform;
+  }
 
   public async addNewToGallery() {
     // Take a photo
@@ -47,25 +54,50 @@ export class PhotoService {
 
     // Use webPath to display the new image instead of base64 since it's
     // already loaded into memory
-    return {
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath
-    };
-  }
 
+    if (this.platform.is('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    }
+    else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+
+      return {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath
+      };
+    }
+  }
   private async readAsBase64(cameraPhoto: CameraPhoto) {
     // Fetch the photo, read as a blob, then convert to base64 format
-    const response = await fetch(cameraPhoto.webPath!);
-    const blob = await response.blob();
 
-    return await this.convertBlobToBase64(blob) as string;
+    // "hybrid" will detect Cordova or Capacitor
+    if (this.platform.is('hybrid')) {
+      // Read the file into base64 format
+      const file = await Filesystem.readFile({
+        path: cameraPhoto.path
+      });
+
+      return file.data;
+    }
+    else {
+      const response = await fetch(cameraPhoto.webPath!);
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
   }
-
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
     const reader = new FileReader;
     reader.onerror = reject;
     reader.onload = () => {
-        resolve(reader.result);
+      resolve(reader.result);
     };
     reader.readAsDataURL(blob);
   });
@@ -75,20 +107,26 @@ export class PhotoService {
     const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photoList.value) || [];
 
-    // Display the photo by reading into base64 format
-for (let photo of this.photos) {
-  // Read each saved photo's data from the Filesystem
-  const readFile = await Filesystem.readFile({
-      path: photo.filepath,
-      directory: FilesystemDirectory.Data
-  });
+    // Easiest way to detect when running on the web:
+    // “when the platform is NOT hybrid, do this”
+    if (!this.platform.is('hybrid')) {
 
-  // Web platform only: Load the photo as base64 data
-  photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
-}
+      // Display the photo by reading into base64 format
+      for (let photo of this.photos) {
+        // Read each saved photo's data from the Filesystem
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+        });
+
+
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
+    }
   }
-}
 
+}
 export interface Photo {
   filepath: string;
   webviewPath: string;
